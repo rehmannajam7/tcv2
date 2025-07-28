1c0cd814b175dc5bf97fb85c9701acc261806c6f
Najam Rehman <rehmannajam7@gmail.com>
2025-07-28 15:10:19 +0000

    Update: Update Livechat widget bubble

--- app/javascript/sdk/sdk.js
+++ app/javascript/sdk/sdk.js
@@ -45,11 +45,11 @@ export const SDK_CSS = `
   bottom: 20px;
   box-shadow: 0 8px 24px rgba(0, 0, 0, .16) !important;
   cursor: pointer;
-  height: 64px;
+  height: 60px;
   padding: 0px;
   position: fixed;
   user-select: none;
-  width: 64px;
+  width: 60px;
   z-index: 2147483000 !important;
   overflow: hidden;
 }
@@ -135,9 +135,9 @@ export const SDK_CSS = `
 
 .woot-widget-bubble svg {
   all: revert;
-  height: 24px;
-  margin: 20px;
-  width: 24px;
+  height: 45px;
+  margin: 10px;
+  width: 35px;
 }
 
 .woot-widget-bubble.woot-widget-bubble-color--lighter path{
