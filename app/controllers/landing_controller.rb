3e5ad906e13885ce3083f637857bbef40920f004
Najam Rehman <rehmannajam7@gmail.com>
2025-08-03 10:44:20 +0000

    Add initial landing page functionality

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,14 @@
+class LandingController < ApplicationController
+  skip_before_action :set_current_user
+  skip_before_action :verify_authenticity_token
+  layout 'landing'
+
+  def index
+    # Landing page logic here
+    @global_config = GlobalConfig.get(
+      'LOGO', 'LOGO_DARK', 'LOGO_THUMBNAIL',
+      'INSTALLATION_NAME',
+      'BRAND_URL', 'BRAND_NAME'
+    )
+  end
+end
