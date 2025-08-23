bd54a65187dd36eb7da5b9c5f5a4edd831e9a2ae
Najam Rehman <rehmannajam7@gmail.com>
2025-08-23 06:24:09 +0000

    Fix trailing whitespace in simple_page_crawl_service

--- enterprise/app/services/captain/tools/simple_page_crawl_service.rb
+++ enterprise/app/services/captain/tools/simple_page_crawl_service.rb
@@ -32,24 +32,33 @@ class Captain::Tools::SimplePageCrawlService
   def extract_links_from_html
     @doc.xpath('//a/@href').filter_map do |link|
       href = link.value.to_s.strip
+      next if invalid_href?(href)
 
-      # Skip empty, anchor-only, or non-HTTP(S) schemes
-      next if href.blank? || href.start_with?('#') ||
-              href.start_with?('tel:', 'mailto:', 'javascript:', 'ftp:')
-
-      begin
-        # Try to create absolute URL
-        absolute_url = URI.join(@external_link, href).to_s
-
-        # Validate the resulting URL is HTTP(S)
-        parsed_url = URI.parse(absolute_url)
-        next unless parsed_url.is_a?(URI::HTTP) || parsed_url.is_a?(URI::HTTPS)
-
-        absolute_url
-      rescue URI::InvalidURIError => e
-        Rails.logger.warn("Skipping invalid URI: #{href} - #{e.message}")
-        next
-      end
+      create_absolute_url(href)
     end.to_set
+  end
+
+  def invalid_href?(href)
+    href.blank? || href.start_with?('#') || non_http_scheme?(href)
+  end
+
+  def non_http_scheme?(href)
+    href.start_with?('tel:', 'mailto:', 'javascript:', 'ftp:')
+  end
+
+  def create_absolute_url(href)
+    absolute_url = URI.join(@external_link, href).to_s
+    parsed_url = URI.parse(absolute_url)
+
+    return absolute_url if valid_http_url?(parsed_url)
+
+    nil
+  rescue URI::InvalidURIError => e
+    Rails.logger.warn("Skipping invalid URI: #{href} - #{e.message}")
+    nil
+  end
+
+  def valid_http_url?(parsed_url)
+    parsed_url.is_a?(URI::HTTP) || parsed_url.is_a?(URI::HTTPS)
   end
 end
