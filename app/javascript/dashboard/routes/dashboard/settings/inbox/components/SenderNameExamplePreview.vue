156624a7a1c784324ef4eb9854e65ab421cfccd7
Najam Rehman <rehmannajam7@gmail.com>
2025-08-24 20:47:36 +0000

    update app name

--- app/javascript/dashboard/routes/dashboard/settings/inbox/components/SenderNameExamplePreview.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/components/SenderNameExamplePreview.vue
@@ -31,7 +31,7 @@ export default {
           ),
           preview: {
             senderName: 'Smith',
-            businessName: 'Chatwoot',
+            businessName: 'ThumbCrowd',
             email: '<support@yourbusiness.com>',
           },
         },
@@ -45,7 +45,7 @@ export default {
           ),
           preview: {
             senderName: '',
-            businessName: 'Chatwoot',
+            businessName: 'ThumbCrowd',
             email: '<support@yourbusiness.com>',
           },
         },
